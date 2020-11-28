6d4379b1fa990531adaa911b2c24e4f5fb34e6fd
Thomas Coats <github@voodoolabs.net>
2020-11-28 22:30:27 +1300

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const inject = require('seacreature/lib/inject')
+const { parentPort } = require('worker_threads')
+const net = require('net')
+const readline = require('readline')
+const pjson = require('./package.json')
+
+inject('ctx', async () => {
+  const netServer = net.createServer(async socket => {
+    const rl = readline.createInterface({ input: socket, crlfDelay: Infinity })
+    for await (const line of rl) console.log(line)
+  })
+
+  netServer.on('error', e => console.error(e))
+  return { netServer }
+})
+
+inject('pod', async ({ netServer, hub, startup }) => {
+  const release = startup.retain()
+  const port = process.env.PORT || 8081
+  netServer.listen(port, () => {
+    release()
+    const { address, port } = netServer.address()
+    hub.on('shutdown', () => netServer.terminate())
+    console.log(`${pjson.name}@${pjson.version} ${address}:${port}`)
+    if (parentPort)
+      parentPort.postMessage(JSON.stringify({ e: 'enable_caddy_logging', p: { port } }))
+  })
+})
---
+++ index.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+require('./lib/hub')
+require('./lib/health')
+require('./app')
+require('./lib/plumbing')
